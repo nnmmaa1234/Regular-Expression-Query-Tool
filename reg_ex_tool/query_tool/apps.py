db965148da250d3743272ae5b665efa8e6fff10d
Nooriya Mehmood <57182904+nnmmaa1234@users.noreply.github.com>
2022-09-07 10:36:31 +0500

    added project folders

---
+++ reg_ex_tool/query_tool/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class QueryToolConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "query_tool"
---
+++ reg_ex_tool/query_tool/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class RegexForm(forms.Form):
+    string_input = forms.CharField(label='String', max_length=500)
+    regex_input = forms.CharField(label='Regex', max_length=200)
